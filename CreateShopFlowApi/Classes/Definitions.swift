f6d6593461f094d0466bbe1c11046cffd197e55b
morapelker <morapelker@gmail.com>
2022-07-06 15:20:48 +0300

    "v1.0.3"

--- CreateShopFlowApi/Classes/Definitions.swift
+++ CreateShopFlowApi/Classes/Definitions.swift
@@ -23,8 +23,56 @@ public enum AddShopError: Error {
     case flowCancelled
 }
 
+
+public struct EditShopFlowRequest {
+    
+    
+    public let id: String
+    public let name: String
+    public let description: String?
+    public let shopNumber: String?
+    public let keywords: [String]
+    public let companyType: String
+    public let categories: [String]
+    public let deliveryMethods: [String]
+    public let countries: [String]
+    public let qtyOptions: String?
+    
+    public let phoneNumbers: [String]
+    public let websites: [String]
+    public let email: String?
+    
+    public let foundedCountry: String
+    public let address: String?
+    public let city: String?
+    
+    public let coverImage: String?
+    
+    public init(id: String, name: String, description: String?, shopNumber: String?, keywords: [String], companyType: String, categories: [String], deliveryMethods: [String], countries: [String], qtyOptions: String?, phoneNumbers: [String], websites: [String], email: String?, foundedCountry: String, address: String?, city: String?, coverImage: String?) {
+        self.id = id
+        self.name = name
+        self.description = description
+        self.shopNumber = shopNumber
+        self.keywords = keywords
+        self.companyType = companyType
+        self.categories = categories
+        self.deliveryMethods = deliveryMethods
+        self.countries = countries
+        self.qtyOptions = qtyOptions
+        self.phoneNumbers = phoneNumbers
+        self.websites = websites
+        self.email = email
+        self.foundedCountry = foundedCountry
+        self.address = address
+        self.city = city
+        self.coverImage = coverImage
+    }
+}
+
 public protocol CreateShopFlowApi {
         
     func startFlow(in viewController: UIViewController, di: Container) -> AnyPublisher<AddShopResult, AddShopError>
+    
+    func startEditFlow(in viewController: UIViewController, di: Container, request: EditShopFlowRequest) -> AnyPublisher<AddShopResult, AddShopError>
     
 }
