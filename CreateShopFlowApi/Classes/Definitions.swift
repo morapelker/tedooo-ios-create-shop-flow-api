8ae1a4fb62ed88c5a8de2954c8dce747610b1b48
morapelker <morapelker@gmail.com>
2022-07-07 19:35:39 +0300

    "v1.0.11"

--- CreateShopFlowApi/Classes/Definitions.swift
+++ CreateShopFlowApi/Classes/Definitions.swift
@@ -73,9 +73,8 @@ public struct EditShopFlowRequest {
 
 public protocol CreateShopFlowApi {
         
-    func startFlow(in viewController: UIViewController) -> AnyPublisher<AddShopResult, AddShopError>
+    func startFlow(in viewController: UIViewController, fromOnBoarding: Bool) -> AnyPublisher<AddShopResult, AddShopError>
     
     func startEditFlow(in viewController: UIViewController, request: EditShopFlowRequest) -> AnyPublisher<AddShopResult, AddShopError>
- 
-    
+     
 }
